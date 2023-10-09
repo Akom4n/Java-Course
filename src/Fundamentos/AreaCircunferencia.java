3680d3a35d72731cd9ac4f8b1f9c947cfc0ccf30
Vinicius Silva <xdenik13@gmail.com>
2023-10-09 15:23:13 -0300

    Variaveis e constantes

---
+++ src/Fundamentos/AreaCircunferencia.java
@@ -0,0 +1,23 @@
+package Fundamentos;
+
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AreaCircunferencia {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        double raio = 3.4;
+        final double PI = 3.14159;
+
+        double area = PI * raio * raio;
+        System.out.println(area);
+
+        raio = 10;
+        area = PI * raio * raio;
+        System.out.println("Área = " + area + "m2.");
+
+    }
+}
