6d785f77c891685f120152232b7564ded54752bd
Vinicius Silva <xdenik13@gmail.com>
2023-10-10 15:32:47 -0300

    Create class

---
+++ src/Fundamentos/Temperatura.java
@@ -0,0 +1,21 @@
+package Fundamentos;
+
+import java.util.Scanner;
+
+public class Temperatura {
+
+    public static void main(String[] args) {
+        // (F - 32) x 5/9 = C
+        Scanner sc = new Scanner(System.in);
+
+        final double FATOR = 5.0 / 9.0;
+        final double AJUSTE = 32;
+        double fahrnheit = 86;
+        double celsius = (fahrnheit - AJUSTE) * FATOR;
+
+        fahrnheit = 150;
+        celsius = (fahrnheit - AJUSTE) * FATOR;
+
+        System.out.println("O resultado é " + celsius + "°C.");
+    }
+}
