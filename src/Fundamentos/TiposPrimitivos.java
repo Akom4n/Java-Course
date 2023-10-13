0d801a119cd23e970c5990d77463f8b1a2affb8d
Vinicius Silva <xdenik13@gmail.com>
2023-10-13 18:25:43 -0300

    Explicando o basico dos tipos primitivos

--- src/Fundamentos/TiposPrimitivos.java
+++ src/Fundamentos/TiposPrimitivos.java
@@ -4,9 +4,13 @@ public class TiposPrimitivos {
 
     /* Sobre tipos primitivos
 
-     Int - tipo inteiro
-
-
+     Tipos inteiros numericos: byte(1), short(2), int(4), long(8) (A diferença primordial entre eles seria a capacidade numerica de cada).
+     
+     Tipos inteiros com pontos flutuantes: float, double (O double é o dobro do float(F4 - D8)
+     
+     Tipo char: representa um caractere, seja uma letra até um simbolo.
+     
+     Tipo boolean : representa sempre false or true(0 ou 1)
 
      */
 }
