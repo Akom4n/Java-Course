50b2c814907719c8d7d21f0a7165c5eea6eb70ac
Vinicius Silva <xdenik13@gmail.com>
2023-11-04 13:39:21 -0300

    Setimo Desafio

---
+++ src/controle/desafios/SetimoDesafio.java
@@ -0,0 +1,22 @@
+package controle.desafios;
+
+import java.util.Scanner;
+
+public class SetimoDesafio {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		int sum = 0;
+		
+		while (num >= 0) {
+			System.out.println("Digite um número inteiro (ou um negativo para sair): ");
+			num = sc.nextInt();
+			if (num >= 0) {
+				sum += num;
+				System.out.printf("\nSoma até o momento: %d\n", num);
+			}
+		}
+		sc.close();
+	}
+}
