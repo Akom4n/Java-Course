5609e977726175caa86e57e06d3f04a5d2210784
Vinicius Silva <xdenik13@gmail.com>
2023-11-20 20:05:32 -0300

    Desafio Construtor

--- src/classe/desafios/Data.java
+++ src/classe/desafios/Data.java
@@ -5,4 +5,22 @@ public class Data {
 	int dia;
 	int mes;
 	int ano;
+	
+	//Desafio aula 103
+	
+	Data(){
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+	
+	Data(int diaInicial, int mesInicial, int anoInicial){
+		dia = diaInicial;
+		mes = mesInicial;
+		ano = anoInicial;
+	}
+	
+	String obterDataFormatada() {
+		return String.format("%d/%d/%d", dia, mes, ano);
+	}
 }
--- src/classe/desafios/DataTeste.java
+++ src/classe/desafios/DataTeste.java
@@ -5,17 +5,23 @@ public class DataTeste {
 	public static void main(String[] args) {
 		
 		Data d1 = new Data();
-		d1.dia = 17;
-		d1.mes = 11;
+//		d1.dia = 17;
+//		d1.mes = 11;
 		d1.ano = 2023;
 		
-		System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
+//		System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
 		
-		var d2 = new Data();
-		d2.dia = 31;
-		d2.mes = 12;
-		d2.ano = 2023;
+		var d2 = new Data(31, 12, 2020);
+//		d2.dia = 31;
+//		d2.mes = 12;
+//		d2.ano = 2023;
 		
-		System.out.printf("%d/%d/%d", d2.dia, d2.mes, d2.ano);
+//		System.out.printf("%d/%d/%d", d2.dia, d2.mes, d2.ano);
+		
+		String dataFormatada1 = d1.obterDataFormatada();
+		System.out.println(dataFormatada1);	
+		
+		String dataFormatada2 = d2.obterDataFormatada();
+		System.out.println(dataFormatada2);
 	}
 }
