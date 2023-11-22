2ee06eda9261eb82d371099cf7516a74f5ee764e
Vinicius Silva <xdenik13@gmail.com>
2023-11-21 22:43:04 -0300

    Desafio Membros de classe vs Instância

--- src/classe/Produto.java
+++ src/classe/Produto.java
@@ -4,5 +4,24 @@ public class Produto {
 	
 	String nome;
 	double preco;
-	double desconto; //Sempre entre 0 e 1 (EX: 0.1 = 10% de desconto)
+	static double desconto = 0.25; //Sempre entre 0 e 1 (EX: 0.1 = 10% de desconto)
+	
+	//Modificando para desafio Membros de classe vs Instancia
+	
+	Produto(){
+		
+	}
+	
+	Produto(String nomeInicial, double precoInicial){
+		nome = nomeInicial;
+		preco = precoInicial;
+	}
+	
+	double precoComDesconto() {
+		return preco * (1 - desconto);
+	}
+	
+	double precoComDesconto(double descontoDoGerente) {
+		return preco * (1 - desconto * descontoDoGerente);
+	}
 }
